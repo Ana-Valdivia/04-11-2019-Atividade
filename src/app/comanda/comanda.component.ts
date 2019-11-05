47d75a2f5b84d1f84b73178223d1d3a75bfb7c96
ana-valdivia <ana.raquel1291@hotmail.com>
2019-11-04 23:09:14 -0200

    Exercicio 8

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -8,7 +8,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   styleUrls: ['./comanda.component.css']
 })
 export class ComandaComponent implements OnInit {
-  
+
   formCliente: FormGroup;
 
   constructor(public formBuilder: FormBuilder) { }
@@ -17,6 +17,47 @@ export class ComandaComponent implements OnInit {
   
 
   ngOnInit() {
-  }
 
+    this.formCliente =this.formBuilder.group({
+       inChoop :this.formBuilder.control(''),
+       inPizza :this.formBuilder.control(''),
+       inRecheio :this.formBuilder.control(''),
+       inPessoas :this.formBuilder.control(''),
+       
+    })
+
+  }
+  onProcessar(){
+
+    let totalChopp:number =   this.formCliente.value.chopp * 7.30;
+    let totalPizza:number =   this.formCliente.value.pizza * 31.50;
+    let totalRecheio:number =   this.formCliente.value.recheio * 5.90;
+    let totalPessoa:number =   this.formCliente.value.pessoa;
+    let valorTaxa: number = 0;   
+
+    let totalTotal: number = totalChopp + totalPizza + totalRecheio;
+
+    let resulTaxa: boolean = this.formCliente.value.taxaservico;
+
+    if(resulTaxa == true)
+    {
+        valorTaxa = totalTotal * 0.1;        
+        
+    }
+    else
+    {
+      valorTaxa = 0;
+    
+    }
+
+
+    let totalAPagar: number = totalTotal + valorTaxa;
+    let porPessoa: number = totalAPagar / totalPessoa;
+
+    alert(`Total: ${totalTotal} \n Valor Taxa: ${valorTaxa} \nTotal com Taxa: ${totalAPagar} \nTotal Por pessoa: ${porPessoa}`);
+
+
+  
+  }
+     
 }
